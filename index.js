f82ef19eea4bb0ff01a76f6326a683c8455081c4
8E0WxZmJU42n6UQ2AL <58239272+8E0WxZmJU42n6UQ2AL@users.noreply.github.com>
2019-11-27 02:17:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-lol
+<script src="https://www.google.com/recaptcha/api.js?render=_reCAPTCHA_site_key"></script>
+<script>
+grecaptcha.ready(function() {
+    grecaptcha.execute('_reCAPTCHA_site_key_', {action: 'homepage'}).then(function(token) {
+       ...
+    });
+});
+</script>
+  
+  <script>
+grecaptcha.ready(function() {
+    grecaptcha.execute('reCAPTCHA_site_key', {action: 'https://impact-project.github.io/discord'});
+});
+</script>
